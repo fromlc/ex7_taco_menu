7c5cc872af513641abdf4c2e0ab266deca07aacc
L Carver <wifihum@gmail.com>
2023-10-08 20:14:44 -0700

    Add project files.

---
+++ ex7_taco_menu.cpp
@@ -0,0 +1,142 @@
+//------------------------------------------------------------------------------
+// ex7_taco_menu.cpp
+//
+// Practice char input menu structure
+//------------------------------------------------------------------------------
+#include <iostream>
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+const string MENU_STR = "F)ish, S)alsa, P)ico de gallo, L)ettuce, A)vocado Q)uit: ";
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+void setup();
+char get_menu_option();
+void display_fish_filling();
+void display_salsa_filling();
+void display_pico_filling();
+void display_lettuce_filling();
+void display_avocado_filling();
+void display_error();
+void display_taco_ready();
+
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+int main() {
+
+    setup();
+
+    char option;
+    while (true) {
+        option = get_menu_option();
+
+        if (option == 'F')
+            display_fish_filling();
+        else if (option == 'S')
+            display_salsa_filling();
+        else if (option == 'P')
+            display_pico_filling();
+        else if (option == 'L')
+            display_lettuce_filling();
+        else if (option == 'A')
+            display_avocado_filling();
+        else if (option == 'Q')
+            break;
+        else
+            display_error();
+
+    } while (option != 'Q');
+
+    display_taco_ready();
+
+    return 0;
+}
+
+
+//------------------------------------------------------------------------------
+// display app banner and instructions
+//------------------------------------------------------------------------------
+void setup() {
+
+    cout << "\nProf. Linda C's Taco Shop";
+    cout << "\n\nChoose your taco fillings!\n";
+}
+
+//------------------------------------------------------------------------------
+// display taco menu options
+//------------------------------------------------------------------------------
+char get_menu_option() {
+
+    char topping;
+    cout << '\n' << MENU_STR;
+    cin >> topping;
+    return toupper(topping);
+}
+
+//------------------------------------------------------------------------------
+// add fish to taco order
+//------------------------------------------------------------------------------
+void display_fish_filling() {
+
+    cout << "Adding freshly caught mahi-mahi\n";
+}
+
+//------------------------------------------------------------------------------
+// add salsa to taco order
+//------------------------------------------------------------------------------
+void display_salsa_filling() {
+
+    cout << "Adding HOT homemade salsa\n";
+}
+
+//------------------------------------------------------------------------------
+// add pico de gallo to taco order
+//------------------------------------------------------------------------------
+void display_pico_filling() {
+
+    cout << "Adding diced tomato, onion, and chopped cilantro\n";
+
+}
+
+//------------------------------------------------------------------------------
+// add shredded lettuce to taco order
+//------------------------------------------------------------------------------
+void display_lettuce_filling() {
+
+    cout << "Adding crisp, shredded iceberg lettuce\n";
+
+}
+
+//------------------------------------------------------------------------------
+// add sliced avocado to taco order
+//------------------------------------------------------------------------------
+void display_avocado_filling() {
+
+    cout << "Adding creamy avocado slices\n";
+
+}
+
+//------------------------------------------------------------------------------
+// menu option not offered
+//------------------------------------------------------------------------------
+void display_error() {
+
+    cerr << "Sorry, that filling is not available.\n";
+
+}
+
+
+//------------------------------------------------------------------------------
+// cleanup tasks
+//------------------------------------------------------------------------------
+void display_taco_ready() {
+
+    cout << "Sounds delicious! Enjoy your taco.\n";
+
+}
